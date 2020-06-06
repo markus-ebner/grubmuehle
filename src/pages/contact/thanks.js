c2f77e71a999f4c384e9bef4c2395c24a67b088c
Markus Ebner <ebner.markus94@gmail.com>
2020-06-06 21:57:13 +0200

    translate, layout footer, logo

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Layout from '../../components/Layout'
+import React from "react";
+import Layout from "../../components/Layout";
 
 export default () => (
   <Layout>
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>Danke für deine Anfrage!</h1>
+          <p>Wir werden uns so schnell wie möglich bei dir melden!</p>
         </div>
       </div>
     </section>
   </Layout>
-)
+);
