6d5ea343a7c5232f5ef7ab7abb6de2e3742cac2a
Markus Ebner <ebner.markus94@gmail.com>
2020-06-18 18:22:10 +0200

    adapt logos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 
 // import logo from "../img/logo.svg";
-import logo from "../img/logo.svg";
+import logo from "../img/logo_reversed.svg";
 
 const Footer = class extends React.Component {
   render() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,11 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Zur Grubmühle" style={{ width: "120px" }} />
+              <img
+                src={logo}
+                alt="Zur Grubmühle"
+                style={{ width: "100px", maxHeight: "none" }}
+              />
             </Link>
             {/* Hamburger menu */}
             <div
