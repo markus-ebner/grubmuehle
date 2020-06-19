ad41f0e8a45897ea6ce0bc4ac6e235dcd465d615
Markus Ebner <ebner.markus94@gmail.com>
2020-06-19 14:44:18 +0200

    change text width

--- src/templates/apartment-page.js
+++ src/templates/apartment-page.js
@@ -27,7 +27,7 @@ export const ApartmentPageTemplate = ({
       <div className="container">
         <div className="section">
           <div className="columns">
-            <div className="column is-7 is-offset-1">
+            <div className="column is-10 is-offset-1">
               <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
               <p>{description}</p>
             </div>
@@ -36,7 +36,7 @@ export const ApartmentPageTemplate = ({
             <div className="column is-10 is-offset-1">
               <Features gridItems={intro.blurbs} />
               <div className="columns">
-                <div className="column is-7">
+                <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-3">
                     {main.heading}
                   </h3>
