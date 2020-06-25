461a07832b474432934347649d3f44e37053cf1f
Markus Ebner <ebner.markus94@gmail.com>
2020-06-25 11:28:30 +0200

    remove warnings

--- src/templates/apartment-page.js
+++ src/templates/apartment-page.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
-import Features from "../components/Features";
 import { Room } from "../components/Room";
 import { MainHeading } from "../components/MainHeading";
 
